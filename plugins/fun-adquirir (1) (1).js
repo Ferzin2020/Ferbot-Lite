4c5613315ea1d7d20621ef7d0f42e261efea6150
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-07-22 14:38:43 +0200

    Actualizar fun-adquirir (1) (1).js

--- plugins/fun-adquirir (1) (1).js
+++ plugins/fun-adquirir (1) (1).js
@@ -10,16 +10,16 @@ m.reply(`*Precios del bot*
 
 _*PRECIOS*_
 
-• 1 BOT 2USD
-• 2 BOT 4USD
-• 3 BOT 6USD
-• 4 BOT 8USD
-• 5 BOT 10USD
+• 1 BOT 3USD
+• 2 BOT 6USD
+• 3 BOT 9USD
+• 4 BOT 10USD
+• 5 BOT 12USD
 
 • BOT PERSONALIZADOS 20USD
 
 *🛒|• +300 COMANDOS INFO AL PRIVADO DE :*
-Envía un mensaje a @iambuitre por WhatsApp. https://wa.me/message/MBTBZMTEGVEHL1
+ https://wa.me/message/MBTBZMTEGVEHL1
 
 METODO DE PAGO: 
 PAYPAL Y TARJETA DE CRÉDITO O DEBITO`, null, {
